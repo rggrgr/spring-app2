593603ce24c2f084b07fc4f8ea40995fe69868e4
rggrgr <aman.dyachkovskiy.00@mail.ru>
2023-11-03 22:33:01 +0900

    lesson24

--- src/main/java/ru/alishev/springcourse/models/Person.java
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -1,14 +1,49 @@
 package ru.alishev.springcourse.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min=2, max=30, message = "Name should be between 2 and 30 characters")
     private String name;
 
-    public Person(){}
+    @Min(value = 0, message = "Age should be greater than 0")
+    private int age;
 
-    public Person(int id, String name) {
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should ne valid")
+    private String email;
+
+    public Person(){
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
 
     public int getId() {
